5b739b317204f69a5967dc94a6d1e5962c31798f
Michelle.Watts22 <michelle.watts22@det.nsw.edu.au>
2023-05-30 10:17:21 +1000

    promptopia files

---
+++ app/layout.jsx
@@ -0,0 +1,29 @@
+import "@styles/globals.css";
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
+
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover & Share AI Propts",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+
+          <main className="app">
+            <Nav key="123" />
+            {children}
+          </main>
+        </Provider>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
